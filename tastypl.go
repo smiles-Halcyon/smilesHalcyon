57d78a755f686a1d83b30ea9fad78bfa00b3a724
smilesHalcyon <smiles@halcyoncapital.org>
2018-11-06 02:59:27 -0600

    Summary Notes

--- tastypl.go
+++ tastypl.go
@@ -1,17 +1,10 @@
-// Copyright (C) 2017  Go Tasty
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+test "" = "$(grep '^Signed-off-by: ' "$1" |
+	 sort | uniq -c | sed -e '/^[ 	]*1[ 	]/d')" || {
+	echo >&2 Duplicate Signed-off-by lines.
+	exit 1
+}
+
+$ go run tastypl.go -input demo.csv -printpl -positions
 
 package main
 
